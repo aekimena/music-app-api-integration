034b63329a38169572b3c7f3edf09ccaa7626d56
Kimena <realarinzemmanuel@gmail.com>
2023-07-04 09:13:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,18 +15,18 @@ let inputValve = document.getElementById('search-form');
 let activeValueReturned = 'track';
 let searchArrayValue = 'tracks';
 
-function search(){
+function search() {
     document.getElementById('search-form').addEventListener('focus', () => {
         document.addEventListener('keyup', checkKeyPressed);
     })
 }
 
-function checkType(){
+function checkType() {
     const activeType = document.querySelectorAll('.dropdown-menu li');
     activeType.forEach(listItem => {
         listItem.addEventListener('click', () => {
             listItem.classList.add('active')
-            let sibllings  = Array.from(listItem.parentNode.children);
+            let sibllings = Array.from(listItem.parentNode.children);
             let filteredSiblings = sibllings.filter((element) => {
                 return element !== listItem;
             })
@@ -34,34 +34,36 @@ function checkType(){
                 unwantedList.classList.remove('active');
                 console.log(unwantedList)
             })
-            if(listItem.classList.contains('active') && listItem.innerHTML == 'Song'){
+            if (listItem.classList.contains('active') && listItem.innerHTML == 'Song') {
                 activeValueReturned = 'track';
                 searchArrayValue = 'tracks';
-            } else if (listItem.classList.contains('active') && listItem.innerHTML == 'Album'){
+            } else if (listItem.classList.contains('active') && listItem.innerHTML == 'Album') {
                 activeValueReturned = 'album';
                 searchArrayValue = 'albums';
-            } else if (listItem.classList.contains('active') && listItem.innerHTML == 'Artist'){
+            } else if (listItem.classList.contains('active') && listItem.innerHTML == 'Artist') {
                 activeValueReturned = 'artist';
                 searchArrayValue = 'artists';
             }
-            re_Search();
+            if (inputValve.value.length !== 0) {
+                re_Search();
+            }
         })
     })
 }
 
 checkType();
 
-function re_Search(){
+function re_Search() {
     async function authenticate() {
         const response = await fetch(TOKEN_URL, {
-        method: "POST",
-        headers: {
-        Authorization: `Basic ${AUTH_HEADER}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: "grant_type=client_credentials",
+            method: "POST",
+            headers: {
+                Authorization: `Basic ${AUTH_HEADER}`,
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: "grant_type=client_credentials",
         });
-    
+
         const responseJson = await response.json();
         const accessToken = responseJson.access_token;
         return accessToken;
@@ -69,13 +71,13 @@ function re_Search(){
 
     async function searchActive(query, accessToken) {
         const response = await fetch(`${SEARCH_URL}?q=${encodeURIComponent(query)}&type=${activeValueReturned}&limit=${LIMIT}`,
-        {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-        },
-        }
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    "Content-Type": "application/json",
+                },
+            }
         );
         const responseJson = await response.json();
         return responseJson[searchArrayValue].items;
@@ -85,91 +87,88 @@ function re_Search(){
         .then((token) => {
             return searchActive(SEARCH_QUERY, token);
         })
-        .then ((results) => {
-            updateSearch(results);
+        .then((results) => {
+            try {
+                updateSearch(results);
+            } catch (error) {
+                null
+            }
             document.querySelectorAll('.suggestion-items').forEach(list => {
                 list.addEventListener('click', () => {
                     console.log(results[Array.from(list.parentNode.children).indexOf(list)])
+                    let displayClickDetails = results[Array.from(list.parentNode.children).indexOf(list)];
+                    showClickDetails(displayClickDetails);
+                    inputValve.value = "";
+                    searchDisplayContainer.style.visibility = "hidden";
+                    document.querySelector('.search-details').style.display = 'block';
+                    document.getElementById('search-details').scrollIntoView({ behavior: 'smooth', block: 'end' });
                 })
             })
         })
         .catch((error) => {
             console.error("Error:", error);
-    });
+        });
 }
 
-function checkKeyPressed(event){
-    if(event.keyCode){
+function checkKeyPressed(event) {
+    if (event.keyCode) {
         SEARCH_QUERY = inputValve.value;
         re_Search();
 
     }
-    if(inputValve.value.length == 0){
+    if (inputValve.value.length == 0) {
         searchDisplayContainer.style.visibility = "hidden";
     }
 }
 search();
 
-function updateSearch(results){
+function updateSearch(result) {
     searchDisplayContainer.style.visibility = "visible";
-    while (searchDisplay.firstChild){
+    while (searchDisplay.firstChild) {
         searchDisplay.removeChild(searchDisplay.firstChild);
     }
-    results.forEach(item => {
+    result.forEach(item => {
         let searchList = document.createElement('li');
         searchList.classList.add('suggestion-items');
-        if(searchArrayValue == 'tracks'){
+        if (searchArrayValue == 'tracks') {
             searchList.innerHTML = `<img src="${item.album.images[0].url}" alt=""/> <p class="text-dark">${item.name} by ${item.artists[0].name}</p>`;
-            getArtistName(item.artists[0].id)
-        }else if (searchArrayValue == 'albums'){
+        } else if (searchArrayValue == 'albums') {
             searchList.innerHTML = `<img src="${item.images[0].url}" alt=""/> <p class="text-dark">${item.name} by ${item.artists[0].name}</p>`;
-            getArtistName(item.artists[0].id)
-        } else if(searchArrayValue == 'artists'){
+        } else if (searchArrayValue == 'artists') {
             searchList.innerHTML = `<img src="${item.images[0].url}" alt=""/> <p class="text-dark">${item.name}</p>`;
-            getArtistName(item.id)
         }
         searchDisplay.appendChild(searchList);
     });
-}
+};
 
-function getArtistName(artistId){
-    async function authenticate2(){
-        const response = await fetch(TOKEN_URL, {
-            method: "POST",
-            headers: {
-            Authorization: `Basic ${AUTH_HEADER}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: "grant_type=client_credentials",
-            });
-        
-            const responseJson = await response.json();
-            const accessToken = responseJson.access_token;
-            return accessToken;
+function showClickDetails(clickedIndex) {
+    let genreArray = [];
+    if (activeValueReturned == 'track') {
+        document.querySelector('.search-details-image').innerHTML = `<img src="${clickedIndex.album.images[0].url}" class="img-fluid" alt="" />`
+        document.querySelector('.search-details-info').innerHTML = `<p class="h2">Track:</p>
+        <p class="h1">${clickedIndex.name}</p>
+        <p class="h5">By ${clickedIndex.artists[0].name}</p>
+        <a href="${clickedIndex.external_urls.spotify}" target="blank"> <button class="btn btn-dark mt-2 mb-2">Listen on Spotify</button></a>`
+        console.log(clickedIndex);
+    }
+    else if (activeValueReturned == 'album') {
+        document.querySelector('.search-details-image').innerHTML = `<img src="${clickedIndex.images[0].url}" class="img-fluid" alt="" />`
+        document.querySelector('.search-details-info').innerHTML = `<p class="h2">Album:</p>
+        <p class="h1">${clickedIndex.name}</p>
+        <p class="h5">By ${clickedIndex.artists[0].name}</p>
+        <a href="${clickedIndex.external_urls.spotify}" target="blank"> <button class="btn btn-dark mt-2 mb-2">Listen on Spotify</button></a>`
     }
 
-    async function searchForArtist(accessToken) {
-        const response = await fetch(`https://api.spotify.com/v1/artists/${artistId}`,
-        {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-        },
+    else if (activeValueReturned == 'artist') {
+        for (let i of clickedIndex.genres) {
+            genreArray.push(i)
         }
-        );
-        const responseJson = await response.json();
-        return responseJson;
-    };
-
-    authenticate2()
-        .then((token) => {
-            return searchForArtist(token);
-        })
-        .then ((results) => {
-            console.log(results.images[0].url);
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-    });
+        let genreString = genreArray.join(', ');
+        document.querySelector('.search-details-image').innerHTML = `<img src="${clickedIndex.images[0].url}" class="img-fluid" alt="" />`
+        document.querySelector('.search-details-info').innerHTML = `<p class="h2">Artist:</p>
+        <p class="h1">${clickedIndex.name}</p>
+        <p class="h2">Genres:</p>
+        <p class="h3">${genreString}</p>
+        <a href="${clickedIndex.external_urls.spotify}" target="blank"> <button class="btn btn-dark mt-2 mb-2">Listen on Spotify</button></a>`
+    }
 }
